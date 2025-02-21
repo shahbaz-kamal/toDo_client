a29038f6199fa7cc7660e05b4d1a0e556f56c943
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-02-21 12:02:20 +0600

    tested the server if its okay

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000/",
+//   baseURL: "https://to-do-server-fawn.vercel.app/",
 });
 const UseAxiosSecure = () => {
   return axiosSecure;
