969aa251a333d556d0e4923aad415f6ffabd89ce
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-02-22 16:33:32 +0600

    accessibilty limited to logged in user

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Banner from "./Banner";
 import AddTask from "./AddTask";
+import useAuth from "../../Hooks/useAuth";
 
 const Home = () => {
+  const { user } = useAuth();
   return (
     <div className="">
-      <section className="">
-        <Banner></Banner>
-      </section>
-      <section className="w-11/12 md:w-10/12 mx-auto mt-10">
-        <AddTask></AddTask>
-      </section>
+      {(user && user.email) || (
+        <section className="">
+          <Banner></Banner>
+        </section>
+      )}
+
+      {user && user?.email && (
+        <section className="w-11/12 md:w-10/12 mx-auto mt-14">
+          <AddTask></AddTask>
+        </section>
+      )}
     </div>
   );
 };
